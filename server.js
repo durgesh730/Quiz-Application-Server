c1114f87ca6bd0f2bcd9ac85ea34bbe64d21e7cf
Durgesh <durgeshchaudhary020401@gmail.com>
2023-01-03 12:55:06 +0530

    Starter File

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import morgan from "morgan";
+const app = express()
+import cors from 'cors'
+import {config}  from "dotenv"
+
+// app middleware
+app.use(morgan('tiny'));
+app.use(cors())
+app.use(express.json())
+config();
+
+// application port 
+const port = process.env.PORT || 8080
+
+// routes 
+app.get('/', (req, res)=>{
+    try { 
+         res.json("Get Request")
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+app.listen(port, ()=>{
+    console.log(`server connected to http://localhost:${port}`)
+})
